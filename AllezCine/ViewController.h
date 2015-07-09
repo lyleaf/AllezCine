7c6f8dbf7708c8fe48e0cf784cd1343a13d964ea
Liu Yiling <lyleaf@163.com>
2015-07-10 01:21:18 +0200

    Initial Commit

---
+++ AllezCine/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AllezCine
+//
+//  Created by LIUYiling on 10/07/15.
+//  Copyright (c) 2015 LIUYiling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
